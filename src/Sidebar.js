6f43b855bcaa313459b338098f38a881b8aa1446
jremy813 <jremywatkins@gmail.com>
2020-08-27 18:36:09 -0400

    created project files and added sidebar component

---
+++ src/Sidebar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <Avatar />
+        {/* Message Icon */}
+        {/* Phone Icon */}
+        {/* Person Icon */}
+        {/* Notifcation Bell Icon */}
+      </div>
+      <div>
+        {/* Settings Icon */}
+        {/* Network Icon */}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;

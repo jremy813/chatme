481a33ddf4a667f55616ae4000f2e98c902d8ba3
jremy813 <jremywatkins@gmail.com>
2020-08-27 19:13:56 -0400

    added all icons to sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import "./Sidebar";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,19 +1,27 @@
 import React from "react";
 import "./Sidebar.css";
+import Avatar from "@material-ui/core/Avatar";
+import MessageOutlinedIcon from "@material-ui/icons/MessageOutlined";
+import PhoneOutlinedIcon from "@material-ui/icons/PhoneOutlined";
+import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
+import NotificationsOutlinedIcon from "@material-ui/icons/NotificationsOutlined";
+import SettingsOutlinedIcon from "@material-ui/icons/SettingsOutlined";
+import InputOutlinedIcon from "@material-ui/icons/InputOutlined";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__top">
         <Avatar />
-        {/* Message Icon */}
-        {/* Phone Icon */}
-        {/* Person Icon */}
-        {/* Notifcation Bell Icon */}
+
+        <MessageOutlinedIcon />
+        <PhoneOutlinedIcon />
+        <PersonOutlineOutlinedIcon />
+        <NotificationsOutlinedIcon />
       </div>
-      <div>
-        {/* Settings Icon */}
-        {/* Network Icon */}
+      <div className="sidebar__bottom">
+        <SettingsOutlinedIcon />
+        <InputOutlinedIcon />
       </div>
     </div>
   );
